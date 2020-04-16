1b680fd36466b7825be499569f6d35c846e04b10
Garbulix <garbiec@bartek.me>
2020-04-17 00:07:08 +0200

    Improved comments

--- big_files.sh
+++ big_files.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 function generate_random {
-    # generate random numeric string with given length
+    # generate random numeric string with given length and save it in variable
     random_string=$(cat /dev/urandom | tr -dc '0-9' | fold -w $1 | head -n 1)
 }
 
 function check_if_error_occured {
-    # create a signal to abort operation if some kind of error occured
+    # create a confirmation that some kind of error occured
     if [ $? -ne 0 ]
     then 
         abort_pending_operation=1
@@ -25,32 +25,40 @@ max_chars_in_file=$(( $line_length*$max_lines ))
 
 
 ### variables that will change later
-no_of_files=0
 current_file=""
 random_string=0
 random_filename=""
+# "booleans":
 does_file_exist=1
 abort_pending_operation=0
+# vars for summary:
+no_of_files=0
+approx_number_of_chars=0
 
-
+# performing a loop until error occured
 while [ 0 ]
 do
     ### CREATING A FILE
-    generate_random $filename_length    # generowanie nazwy i sciezki do pliku
-    random_filename=$random_string
+    generate_random $filename_length    # generating name for new file
+    random_filename=$random_string      
     current_file=${path}${random_filename}
     
     does_file_exist=$(ls $path | grep -x "$random_filename" | wc -l)
     
     if [ $does_file_exist -ne 0 ]
     then
-        continue
+        continue    # continue to next iteration to generate new name, because that is used
     fi
     
     touch $current_file
-    check_if_error_occured      # jesli byl blad podczas tworzenia pustego pliku
-    if [ $abort_pending_operation -ne 0 ]; then break; fi
-    no_of_files=$(( $no_of_files+1 ))     # jesli nie bylo bledu, to plik powstal, wiec go liczymy
+    
+    check_if_error_occured              # check error when creating file
+    if [ $abort_pending_operation -ne 0 ]
+    then
+        break
+    fi
+    
+    no_of_files=$(( $no_of_files+1 ))   # there was no errors, so we can count the file
     
     ### FILLING UP THE FILE
     line_count=0
@@ -60,19 +68,24 @@ do
         random_line=$random_string
         
         echo $random_line >> $current_file
-        check_if_error_occured      # jesli brakuje miejsca na kolejne linie
-        if [ $abort_pending_operation -ne 0 ]; then break; fi
-        line_count=$(( $line_count+1 ))     # jesli nie bylo bledu, to linia zostala dopisana
+        check_if_error_occured          # if there is no space left for another line
+        
+        if [ $abort_pending_operation -ne 0 ]
+        then
+            break
+        fi
+        
+        line_count=$(( $line_count+1 ))     # count line, because there was no errors
     done
     if [ $abort_pending_operation -ne 0 ]; then break; fi
 done
 
-### podsumowanie
-no_of_full_files=$(( $no_of_files-1 ))      # liczba w pelni wypelnionych plikow
+### SUMMARY
+no_of_full_files=$(( $no_of_files-1 ))  # last file may not be full
 approx_no_of_chars=$(( $line_count*$line_length + $no_of_full_files*$max_chars_in_file ))
 
 echo
-echo "W lokalizacji utworzono $no_of_files plikow oraz co najmniej $approx_no_of_chars znakow liczbowych."
+echo "There are $no_of_files new files with at least $approx_no_of_chars of characters in them in the directory."
 echo
 
 
--- small_files.sh
+++ small_files.sh
@@ -4,23 +4,25 @@
 path="/PATH/TO/PARTITION_OR_DIRECTORY"   
 ###
 
-random_filename=0     # zmienna do losowo tworzonych nazw
+random_filename=""      #
 
-while [ 0 ]     # rob dopoki nie wystapi blad
+# performing a loop until error occured
+while [ 0 ]     
 do
     random_filename=$(cat /dev/urandom | tr -dc '0-9' | fold -w 16 | head -n 1)   # create random filename
+    
     touch ${path}${random_filename}   # touch or create an empty file with given random name
     
     if [ $? -ne 0 ]     # check touch's error code
     then
-        break   # break while loop if there were writing (touching) error
+        break           # break while-loop if there were writing (touching) error
     fi
     
 done
 
 
-# writing a summary
-no_of_files=$(ls $path | wc -l)
+# SUMMARY
+no_of_files=$(ls $path | wc -l)     # counting up ALL files in directory
 
 echo
 echo "There are $no_of_files files in the directory"
